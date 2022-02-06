bf3ab6cfd93fd4ae70294865563c69a7997b3af5
IvySaskiaSR <ivy.sejas@ucb.edu.bo>
2022-02-06 02:08:47 -0400

    changed to ssh on deploy

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,9 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/IvySaskiaSR/vue-deployment-test.git main:main
+# git push -f https://github.com/IvySaskiaSR/vue-deployment-test.git main:main
+git push -f git@github.com:IvySaskiaSR/vue-deployment-test.git main:main
+
 
 cd -
 #
